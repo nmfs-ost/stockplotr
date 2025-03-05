ddbfae94d58bd5a40704e4ae78ba38c5436d5609
sbreitbart-NOAA <sophie.breitbart@noaa.gov>
2025-03-05 10:37:13 -0500

    Corrected if/else structure

--- R/html_all_figs_tables.R
+++ R/html_all_figs_tables.R
@@ -16,23 +16,23 @@
 #' html_all_figs_tables(rda_dir = getwd(),
 #'                      outdir = getwd())
 #' }
-html_all_figs_tables <- function(
-    rda_dir = getwd(),
-    outdir = getwd()
-    ) {
+html_all_figs_tables <- function(rda_dir = getwd(),
+                                 outdir = getwd()) {
+  if (!dir.exists(fs::path(rda_dir, "rda_files"))) {
+    stop("'rda_files' folder not found. Did you enter the correct argument for rda_dir?")
+  } else {
+    if (!dir.exists(fs::path(outdir, "all_tables_figures"))) {
+      asar::create_tables_doc(subdir = tempdir() ,
+                              include_all = TRUE,
+                              rda_dir = rda_dir)
 
-  asar::create_tables_doc(subdir = tempdir() ,
-                           include_all = TRUE,
-                           rda_dir = rda_dir)
+      asar::create_figures_doc(subdir = tempdir(),
+                               include_all = TRUE,
+                               rda_dir = rda_dir)
 
-  asar::create_figures_doc(subdir = tempdir(),
-                           include_all = TRUE,
-                           rda_dir = rda_dir)
+      tabs_figs_text <- c(readLines(fs::path(tempdir(), "08_tables.qmd")), readLines(fs::path(tempdir(), "09_figures.qmd")))
 
-  tabs_figs_text <- c(readLines(fs::path(tempdir(), "08_tables.qmd")),
-                      readLines(fs::path(tempdir(), "09_figures.qmd")))
-
-  yaml_text <-
+      yaml_text <-
 "---
 title: 'All Tables & Figures'
 format:
@@ -42,22 +42,103 @@ format:
 ---
 "
 
-  new_html_qmd <- c(yaml_text,
-                    tabs_figs_text)
+      new_html_qmd <- c(yaml_text, tabs_figs_text)
 
-  writeLines(new_html_qmd, fs::path(outdir, "all_tables_figures.qmd"))
-  quarto::quarto_render(input = fs::path(outdir, "all_tables_figures.qmd"),
-                        output_file = "all_tables_figures.html"
-                        )
+      writeLines(new_html_qmd,
+                 fs::path(outdir, "all_tables_figures.qmd"))
+      quarto::quarto_render(
+        input = fs::path(outdir, "all_tables_figures.qmd"),
+        output_file = "all_tables_figures.html"
+      )
 
-  # create new folder for the html and qmd files because these can't
-  # be saved there before or while rendering
-  dir.create(fs::path(outdir, "all_tables_figures"))
+      # create new folder for the html and qmd files because these can't
+      # be saved there before or while rendering
+      dir.create(fs::path(outdir, "all_tables_figures"))
 
-  file.rename(from = fs::path("all_tables_figures.html"),
-              to = fs::path(outdir, "all_tables_figures", "all_tables_figures.html"))
+      file.rename(
+        from = fs::path("all_tables_figures.html"),
+        to = fs::path(
+          outdir,
+          "all_tables_figures",
+          "all_tables_figures.html"
+        )
+      )
 
-  file.rename(from = fs::path("all_tables_figures.qmd"),
-              to = fs::path(outdir, "all_tables_figures", "all_tables_figures.qmd"))
+      file.rename(
+        from = fs::path("all_tables_figures.qmd"),
+        to = fs::path(outdir, "all_tables_figures", "all_tables_figures.qmd")
+      )
 
+      message("The html and qmd with all tables and figures were newly created.")
+
+    } else {
+      question1 <- readline(
+        paste0(
+          "The 'all_tables_figures' folder already exists within ",
+          fs::path(outdir),
+          ". Would you like to overwrite the files within this folder? (Y/N)"
+        )
+      )
+
+      if (regexpr(question1, "y", ignore.case = TRUE) == 1) {
+        asar::create_tables_doc(
+          subdir = tempdir() ,
+          include_all = TRUE,
+          rda_dir = rda_dir
+        )
+
+        asar::create_figures_doc(
+          subdir = tempdir(),
+          include_all = TRUE,
+          rda_dir = rda_dir
+        )
+
+        tabs_figs_text <- c(readLines(fs::path(tempdir(), "08_tables.qmd")), readLines(fs::path(tempdir(), "09_figures.qmd")))
+
+        yaml_text <-
+"---
+title: 'All Tables & Figures'
+format:
+  html:
+    toc: true
+    embed-resources: true
+---
+"
+
+        new_html_qmd <- c(yaml_text, tabs_figs_text)
+
+        writeLines(new_html_qmd,
+                   fs::path(outdir, "all_tables_figures.qmd"))
+        quarto::quarto_render(
+          input = fs::path(outdir, "all_tables_figures.qmd"),
+          output_file = "all_tables_figures.html"
+        )
+
+        file.rename(
+          from = fs::path("all_tables_figures.html"),
+          to = fs::path(
+            outdir,
+            "all_tables_figures",
+            "all_tables_figures.html"
+          )
+        )
+
+        file.rename(
+          from = fs::path("all_tables_figures.qmd"),
+          to = fs::path(
+            outdir,
+            "all_tables_figures",
+            "all_tables_figures.qmd"
+          )
+        )
+
+        message(
+          "The html and qmd with all tables and figures were regenerated and overwrote the previous versions."
+        )
+
+      } else {
+        warning("The html and qmd with all tables and figures were not regenerated.")
+      }
+    }
   }
+}
