bf28266eaa7d7d8f90560597794ede35148fd4ef
sbreitbart-NOAA <sophie.breitbart@noaa.gov>
2025-03-05 12:04:36 -0500

    Update wordlist; make test for html_all_figs_tables; update html_all_figs_tables by removing outdir (must be wd for quarto::quarto_render() to work)

---
+++ tests/testthat/test-html_all_figs_tables.R
@@ -0,0 +1,104 @@
+test_that("html_all_figs_tables makes qmd and html files with default rda_dir argument", {
+  # read in sample dataset
+  dat <- utils::read.csv(
+    system.file("resources", "sample_data", "petrale_sole-after_2020.csv", package = "stockplotr")
+  )
+
+  stockplotr::exp_all_figs_tables(
+    dat,
+    end_year = 2030,
+    n_projected_years = 3,
+    recruitment_unit_label = "mt",
+    recruitment_scale_amount = 1,
+    ref_line = "unfished",
+    ref_point = 1000,
+    ref_line_sb = "msy",
+    indices_unit_label = "CPUE",
+    rda_dir = getwd()
+  ) |>
+    suppressWarnings()
+
+
+  html_all_figs_tables()
+
+  testthat::expect(file.exists(fs::path(getwd(), "all_tables_figures", "all_tables_figures.qmd")),
+                   failure_message = "Test fail: 'all_tables_figures.qmd' does not exist")
+  testthat::expect(file.exists(fs::path(getwd(), "all_tables_figures", "all_tables_figures.html")),
+                   failure_message = "Test fail: 'all_tables_figures.html' does not exist")
+
+  # erase temporary testing files
+  unlink(fs::path(getwd(), "all_tables_figures"), recursive = T)
+  file.remove(fs::path(getwd(), "captions_alt_text.csv"))
+  unlink(fs::path(getwd(), "rda_files"), recursive = T)
+  })
+
+test_that("html_all_figs_tables makes qmd and html files with non-default rda_dir argument", {
+  # read in sample dataset
+  dat <- utils::read.csv(
+    system.file("resources", "sample_data", "petrale_sole-after_2020.csv", package = "stockplotr")
+  )
+
+  dir.create(fs::path(getwd(), "data"))
+
+  stockplotr::exp_all_figs_tables(
+    dat,
+    end_year = 2030,
+    n_projected_years = 3,
+    recruitment_unit_label = "mt",
+    recruitment_scale_amount = 1,
+    ref_line = "unfished",
+    ref_point = 1000,
+    ref_line_sb = "msy",
+    indices_unit_label = "CPUE",
+    rda_dir = fs::path(getwd(), "data")
+  ) |>
+    suppressWarnings()
+
+  html_all_figs_tables(rda_dir = fs::path(getwd(), "data"))
+
+  testthat::expect(file.exists(fs::path(getwd(), "all_tables_figures", "all_tables_figures.qmd")),
+                   failure_message = "Test fail: 'all_tables_figures.qmd' does not exist")
+  testthat::expect(file.exists(fs::path(getwd(), "all_tables_figures", "all_tables_figures.html")),
+                   failure_message = "Test fail: 'all_tables_figures.html' does not exist")
+
+  # erase temporary testing files
+  unlink(fs::path(getwd(), "all_tables_figures"), recursive = T)
+  unlink(fs::path(getwd(), "rda_files"), recursive = T)
+  unlink(fs::path(getwd(), "data"), recursive = T)
+})
+
+test_that("html_all_figs_tables triggers message (question) when all_tables_figures folder already present", {
+  # read in sample dataset
+  dat <- utils::read.csv(
+    system.file("resources", "sample_data", "petrale_sole-after_2020.csv", package = "stockplotr")
+  )
+
+  stockplotr::exp_all_figs_tables(
+    dat,
+    end_year = 2030,
+    n_projected_years = 3,
+    recruitment_unit_label = "mt",
+    recruitment_scale_amount = 1,
+    ref_line = "unfished",
+    ref_point = 1000,
+    ref_line_sb = "msy",
+    indices_unit_label = "CPUE",
+    rda_dir = getwd()
+  ) |>
+    suppressWarnings()
+
+  dir.create(fs::path(getwd(), "all_tables_figures"))
+
+  testthat::expect_message(html_all_figs_tables())
+
+  # erase temporary testing files
+  unlink(fs::path(getwd(), "all_tables_figures"), recursive = T)
+  file.remove(fs::path(getwd(), "captions_alt_text.csv"))
+  unlink(fs::path(getwd(), "rda_files"), recursive = T)
+})
+
+test_that("html_all_figs_tables stops if rda_dir not found", {
+
+  testthat::expect_error(html_all_figs_tables())
+
+})
