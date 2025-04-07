e218b6f04ac6c3bff92b5226fe9fe8e0df604951
Sophie Breitbart <sophie.breitbart@noaa.gov>
2025-04-07 16:09:30 -0400

    Landings table (#72)
    * First working draft of landings table, documentation
    
    * Made unit test for landings table, and updated exp_all_figs_tables fxn and test
    
    * Updated pipeline to add units to caption
    
    * Update documentation
    
    * Add in TODO regarding gear type re: https://github.com/nmfs-ost/stockplotr/pull/72#issuecomment-2784473932

---
+++ tests/testthat/test-table_landings.R
@@ -0,0 +1,56 @@
+test_that("table_landings generates plots without errors", {
+  # read in sample dataset
+  dat <- utils::read.csv(
+    system.file("resources", "sample_data", "petrale_sole-after_2020.csv", package = "stockplotr")
+  )
+
+  # expect error-free plot with minimal arguments
+  expect_no_error(
+    stockplotr::table_landings(dat)
+  )
+
+  # expect error-free plot with many arguments
+  expect_no_error(
+    stockplotr::table_landings(
+      dat,
+      unit_label = "metric tons",
+      make_rda = FALSE,
+      rda_dir = getwd()
+    )
+  )
+
+
+  # expect flextable object is returned
+  expect_s3_class(
+    stockplotr::table_landings(
+      dat,
+      unit_label = "metric tons",
+      make_rda = FALSE,
+      rda_dir = getwd()
+    ),
+    "flextable"
+  )
+})
+
+test_that("rda file made when indicated", {
+  # read in sample dataset
+  dat <- utils::read.csv(
+    system.file("resources", "sample_data", "petrale_sole-after_2020.csv", package = "stockplotr")
+  )
+
+  # export rda
+  table_landings(
+    dat,
+    unit_label = "metric tons",
+    make_rda = TRUE,
+    rda_dir = getwd()
+  )
+
+  # expect that both rda_files dir and the landings_table.rda file exist
+  expect_true(dir.exists(fs::path(getwd(), "rda_files")))
+  expect_true(file.exists(fs::path(getwd(), "rda_files", "landings_table.rda")))
+
+  # erase temporary testing files
+  file.remove(fs::path(getwd(), "captions_alt_text.csv"))
+  unlink(fs::path(getwd(), "rda_files"), recursive = T)
+})
