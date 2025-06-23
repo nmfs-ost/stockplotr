36234def5e8f12c47253ca4bcaa4d2774e3ef88a
sbreitbart-NOAA <sophie.breitbart@noaa.gov>
2025-06-23 16:34:19 -0400

    Address R cmd check warnings

--- R/stockplotr-package.R
+++ R/stockplotr-package.R
@@ -11,6 +11,8 @@ globvar <- c(
   "estimate", "estimate_lower", "estimate_upper", "estimate_y", "expected", "fleet",
   "growth_pattern", "label", "max_est", "max_est_yr", "module_name", "predicted",
   "figures_tables_dir", "recruitment", "sex", "spawning_biomass", "topic_label", "total_catch",
-  "type", "uncertainty", "uncertainty_label", "val", "where", "year"
+  "type", "uncertainty", "uncertainty_label", "val", "where", "year",
+  ":=", "Year", "avg", "estimate_orig", "interm", "season", "tables_dir", "time", "total_fish",
+  "years_per_year"
 )
 if (getRversion() >= "2.15.1") utils::globalVariables(globvar)
