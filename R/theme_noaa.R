c91f9dfe6a6900016a024d52104356c1347e4b6d
Schiano-NOAA <samantha.schiano@noaa.gov>
2025-08-06 16:55:59 -0400

    add list surrounding theme. Now allows the user to place + theme_noaa() to any ggplot

--- R/theme_noaa.R
+++ R/theme_noaa.R
@@ -5,15 +5,18 @@
 #' @returns
 #' @export
 #'
-#' @examples
-theme_noaa <- function() {
-  ggplot2::theme(
-    plot.background = ggplot2::element_rect(fill = "transparent"),
-    panel.background = ggplot2::element_rect(fill = "transparent"),
-    panel.grid = ggplot2::element_blank(),
-    panel.border = ggplot2::element_rect(colour = "black", fill = NA, linewidth = 0.5),
-    complete = TRUE
-  ) # +
-    # nmfspalette::scale_color_nmfs() +
-    # nmfspalette::scale_fill_nmfs()
+#' @examples ggplot2::ggplot() + ggplot2::geom_line(ggplot2::aes(x = 1:10, y = 1:10)) + theme_noaa()
+theme_noaa <- function(...) {
+  list(
+    # might not need the ggplot2 dependencies if make this pkg an extension
+    ggplot2::theme(
+      plot.background = ggplot2::element_rect(fill = "transparent"),
+      panel.background = ggplot2::element_rect(fill = "transparent"),
+      panel.grid = ggplot2::element_blank(),
+      panel.border = ggplot2::element_rect(colour = "black", fill = NA, linewidth = 0.5),
+      complete = TRUE
+    ) # +
+      # nmfspalette::scale_color_nmfs() +
+      # nmfspalette::scale_fill_nmfs()
+    )
 }
