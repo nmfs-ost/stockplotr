85aab34f47304ed69c82f8917e150d5b926a0660
Schiano-NOAA <samantha.schiano@noaa.gov>
2025-08-11 14:05:08 -0400

    adjust plot_abundanc_at_age with revamped, modular design

--- R/plot_abundance_at_age.R
+++ R/plot_abundance_at_age.R
@@ -30,18 +30,18 @@
 #'
 #' plot_abundance_at_age(
 #'   dat,
-#'   unit_label = "my_unit",
-#'   scale_amount = 10,
-#'   end_year = 2024,
-#'   make_rda = TRUE,
+#'   group = "area",
+#'   unit_label = "fish",
+#'   scale_amount = 1000,
+#'   make_rda = FALSE,
 #'   figures_dir = getwd()
 #' )
 #' }
 plot_abundance_at_age <- function(
     dat,
+    group = NULL,
     unit_label = "fish",
     scale_amount = 1000,
-    end_year = format(Sys.Date(), "%Y"),
     make_rda = FALSE,
     figures_dir = getwd()) {
   magnitude <- floor(log10(scale_amount))
@@ -67,178 +67,31 @@ plot_abundance_at_age <- function(
   
   abundance_label <- ifelse(
     scale_amount == 1000,
-    yes = glue::glue("Abundance ({unit_mag}{unit_label})"),
-    no = glue::glue("Relative abundance ({unit_mag}{unit_label})")
+    yes = glue::glue("Abundance \n({unit_mag}{unit_label})"),
+    no = glue::glue("Relative abundance")
   )
 
-  b <- dat |>
-    dplyr::filter(
-      label == "abundance",
-      module_name != "proj.N.age", # SS3 and BAM target module names
-      !is.na(age)
-    ) |>
-    dplyr::mutate(
-      estimate = as.numeric(estimate),
-      year = as.numeric(year),
-      age = as.numeric(age),
-      estimate_orig = estimate,
-      estimate = estimate_orig / scale_amount
-    )
-
-  if (dim(b |>
-    dplyr::select(time) |>
-    dplyr::filter(!is.na(time)))[1] > 1) {
-    b <- b |>
-      # dplyr::filter(time %% 1 != 0.5) |>
-      dplyr::group_by(age, year) |>
-      dplyr::summarise(estimate = sum(estimate), .groups = "drop") |>
-      dplyr::ungroup()
-  }
-
-  # create plot-specific variables to use throughout fxn for naming and IDing
-  topic_label <- "pop.naa"
-
-  # identify output
-  fig_or_table <- "figure"
-
-  # check year isn't past end_year if not projections plot
-  check_year(
-    end_year = end_year,
-    fig_or_table = fig_or_table,
-    topic = topic_label
+  b <- prepare_data(
+    dat = dat,
+    label_name = "abundance",
+    geom = "point",
+    group = "age"
   )
-
-  b <- b |>
-    dplyr::filter(year <= end_year)
-
-  total_fish_per_year <- b |>
-    dplyr::group_by(year) |>
-    dplyr::summarise(total_fish = sum(estimate))
-
-  annual_means <- b |>
-    dplyr::group_by(age, year) |>
-    dplyr::summarise(years_per_year = sum(estimate)) |>
-    dplyr::filter(age != 0) |>
-    dplyr::group_by(year) |>
-    dplyr::summarise(interm = sum(age * years_per_year)) |>
-    dplyr::full_join(total_fish_per_year) |>
-    dplyr::mutate(avg = interm / total_fish)
-
-  # Choose number of breaks for x-axis
-  x_n_breaks <- round(length(unique(b[["year"]])) / 10)
-  if (x_n_breaks <= 5) {
-    x_n_breaks <- round(length(unique(b[["year"]])) / 5)
-    if (x_n_breaks <= 2) {
-      x_n_breaks <- round(length(unique(b[["year"]])))
-    }
-  } else if (x_n_breaks > 10) {
-    x_n_breaks <- round(length(unique(b[["year"]])) / 15)
-  }
-
-  # Choose number of major breaks for y-axis
-  y_n_breaks <- round(length(unique(b[["age"]])))
-  if (y_n_breaks > 80) {
-    y_n_breaks <- round(length(unique(b[["age"]])) / 6)
-  } else if (y_n_breaks > 40) {
-    y_n_breaks <- round(length(unique(b[["age"]])) / 3)
-  }
-
-  # Choose number of minor breaks for y-axis
-  y_n_breaks_minor <- as.vector(unique(b$age))
-  if (length(y_n_breaks_minor) > 40) {
-    y_n_breaks_minor <- NULL
-  } else if (length(y_n_breaks_minor) > 20) {
-    y_n_breaks_minor <- y_n_breaks_minor[c(TRUE, FALSE)]
-  }
-
-
-
-  # plot
-  plt <- ggplot2::ggplot() +
-    ggplot2::geom_point(
-      data = b,
-      ggplot2::aes(
-        x = year,
-        y = age,
-        size = estimate
-      ),
-      shape = 21,
-      alpha = 0.3,
-      color = "black",
-      fill = "gray40"
-    ) +
-    ggplot2::scale_size(
-      range = c(0.2, 10),
-      name = abundance_label,
-      labels = scales::label_comma()
-    ) +
-    # add line
-    ggplot2::geom_line(
-      data = annual_means,
-      ggplot2::aes(
-        x = year,
-        y = avg
-      ),
-      linewidth = 1,
-      color = "red"
-    ) +
-    ggplot2::labs(
-      x = "Year",
-      y = "Age"
-    ) +
-    ggplot2::scale_x_continuous(
-      n.breaks = x_n_breaks,
-      guide = ggplot2::guide_axis(minor.ticks = TRUE)
-    ) +
-    ggplot2::scale_y_continuous(
-      minor_breaks = y_n_breaks_minor,
-      n.breaks = y_n_breaks,
-      guide = ggplot2::guide_axis(minor.ticks = TRUE),
-      limits = c(min(b$age), max(b$age))
-    )
-
-  final <- stockplotr::add_theme(plt)
-
+  # Plot data
+  plot <- plot_aa(
+    dat = b,
+    facet = group,
+    label = abundance_label
+  )
   # export figure to rda if argument = T
   if (make_rda == TRUE) {
-    # run write_captions.R if its output doesn't exist
-    if (!file.exists(
-      fs::path(getwd(), "captions_alt_text.csv")
-    )
-    ) {
-      stockplotr::write_captions(
-        dat = dat,
-        dir = figures_dir,
-        year = end_year
-      )
-    }
-
-    # add more key quantities included as arguments in this fxn
-    add_more_key_quants(
+    create_rda(
+      object = plot,
+      topic_label = "pop.naa",
+      fig_or_table = "figure",
       dat,
-      topic = topic_label,
-      fig_or_table = fig_or_table,
-      dir = figures_dir,
-      end_year = end_year,
-      units = unit_label,
-      # ref_pt = ref_point,
-      scaling = scale_amount
-    )
-
-    # extract this plot's caption and alt text
-    caps_alttext <- extract_caps_alttext(
-      topic_label = topic_label,
-      fig_or_table = fig_or_table,
-      dir = figures_dir
-    )
-
-    export_rda(
-      object = final,
-      caps_alttext = caps_alttext,
-      figures_tables_dir = figures_dir,
-      topic_label = topic_label,
-      fig_or_table = fig_or_table
+      unit_label = "mt"
     )
   }
-  final
+  plot
 }
