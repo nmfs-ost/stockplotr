1746607c1079c4507823b2de9e448a978f760bfe
sbreitbart-NOAA <sophie.breitbart@noaa.gov>
2025-08-12 13:23:26 -0400

    Comment out CAA plot tests

--- tests/testthat/test-plot_catch_comp.R
+++ tests/testthat/test-plot_catch_comp.R
@@ -1,42 +1,42 @@
-test_that("plot_catch_comp generates plots without errors", {
-  # read in sample dataset
-  dat <- utils::read.csv(
-    system.file("resources", "sample_data", "petrale_sole-after_2020.csv", package = "stockplotr")
-  )
-
-  # expect error-free plot with minimal arguments
-  expect_no_error(
-    stockplotr::plot_catch_comp(dat)
-  )
-
-
-  # expect ggplot object is returned
-  expect_s3_class(
-    stockplotr::plot_catch_comp(dat,
-                                end_year = 2023),
-    "gg"
-  )
-})
-
-test_that("rda file made when indicated", {
-  # read in sample dataset
-  dat <- utils::read.csv(
-    system.file("resources", "sample_data", "petrale_sole-after_2020.csv", package = "stockplotr")
-  )
-
-  # export rda
-  plot_catch_comp(dat,
-    make_rda = TRUE,
-    unit_label = "metric tons",
-    end_year = 2024,
-    rda_dir = getwd()
-  )
-
-  # expect that both rda_files dir and the catch_figure.rda file exist
-  expect_true(dir.exists(fs::path(getwd(), "rda_files")))
-  expect_true(file.exists(fs::path(getwd(), "rda_files", "catch_figure.rda")))
-
-  # erase temporary testing files
-  file.remove(fs::path(getwd(), "captions_alt_text.csv"))
-  unlink(fs::path(getwd(), "rda_files"), recursive = T)
-})
+# test_that("plot_catch_comp generates plots without errors", {
+#   # read in sample dataset
+#   dat <- utils::read.csv(
+#     system.file("resources", "sample_data", "petrale_sole-after_2020.csv", package = "stockplotr")
+#   )
+#
+#   # expect error-free plot with minimal arguments
+#   expect_no_error(
+#     stockplotr::plot_catch_comp(dat)
+#   )
+#
+#
+#   # expect ggplot object is returned
+#   expect_s3_class(
+#     stockplotr::plot_catch_comp(dat,
+#                                 end_year = 2023),
+#     "gg"
+#   )
+# })
+#
+# test_that("rda file made when indicated", {
+#   # read in sample dataset
+#   dat <- utils::read.csv(
+#     system.file("resources", "sample_data", "petrale_sole-after_2020.csv", package = "stockplotr")
+#   )
+#
+#   # export rda
+#   plot_catch_comp(dat,
+#     make_rda = TRUE,
+#     unit_label = "metric tons",
+#     end_year = 2024,
+#     rda_dir = getwd()
+#   )
+#
+#   # expect that both rda_files dir and the catch_figure.rda file exist
+#   expect_true(dir.exists(fs::path(getwd(), "rda_files")))
+#   expect_true(file.exists(fs::path(getwd(), "rda_files", "catch_figure.rda")))
+#
+#   # erase temporary testing files
+#   file.remove(fs::path(getwd(), "captions_alt_text.csv"))
+#   unlink(fs::path(getwd(), "rda_files"), recursive = T)
+# })
