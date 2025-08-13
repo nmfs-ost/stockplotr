6a7689a5ea4f02309e697e0614201c4e57a5af5f
Schiano-NOAA <samantha.schiano@noaa.gov>
2025-08-13 10:48:28 -0400

    Revert "add in spawn rec changes plus adjustments to prepare_data when environment is not interactive"
    This reverts commit 32b93d222282a01a37b3bcab19259e6ba5a3f349.

--- R/plot_spawn_recruitment.R
+++ R/plot_spawn_recruitment.R
@@ -24,56 +24,116 @@ plot_spawn_recruitment <- function(
     dat,
     spawning_biomass_label = "mt",
     recruitment_label = "mt",
+    end_year = format(Sys.Date(), "%Y"),
     make_rda = FALSE,
     figures_dir = getwd()) {
   # Extract recruitment
-  recruitment <- prepare_data(
-    dat = dat,
-    label_name = "recruitment",
-    geom = "point"
-  ) |>
-  dplyr::rename(recruitment = estimate) |>
-  dplyr::select(-c(label, estimate_lower, estimate_upper))
+  rec <- dat |>
+    dplyr::filter(
+      label == "recruitment",
+      module_name == "TIME_SERIES" | module_name == "t.series",
+      !is.na(year),
+      is.na(fleet) | length(unique(fleet)) <= 1,
+      is.na(sex) | length(unique(sex)) <= 1,
+      is.na(area) | length(unique(area)) <= 1,
+      is.na(growth_pattern) | length(unique(growth_pattern)) <= 1,
+      !year %in% year_exclusions
+    ) |> # SS3 and BAM target module names
+    dplyr::mutate(
+      estimate = as.numeric(estimate),
+      year = as.numeric(year)
+    ) |>
+    dplyr::rename(recruitment = estimate) |>
+    dplyr::select(-c(module_name, label))
+
+  stryr <- min(rec$year)
+
+  # create plot-specific variables to use throughout fxn for naming and IDing
+  topic_label <- "sr"
+
+  # identify output
+  fig_or_table <- "figure"
+
+  # check year isn't past end_year if not projections plot
+  check_year(
+    end_year = end_year,
+    fig_or_table = fig_or_table,
+    topic = topic_label
+  )
 
   # Extract spawning biomass
-  sb <- prepare_data(
-    dat = dat,
-    label_name = "spawning biomass",
-    geom = "point"
-  ) |>
-  dplyr::rename(spawning_biomass = estimate) |>
-  dplyr::select(-c(label, estimate_lower, estimate_upper))
+  sb <- dat |>
+    dplyr::filter(
+      label == "spawning_biomass",
+      module_name == "TIME_SERIES" | module_name == "t.series",
+      !is.na(year),
+      is.na(fleet) | length(unique(fleet)) <= 1,
+      is.na(sex) | length(unique(sex)) <= 1,
+      is.na(area) | length(unique(area)) <= 1,
+      is.na(growth_pattern) | length(unique(growth_pattern)) <= 1,
+      !year %in% year_exclusions
+    ) |> # SS3 and BAM target module names
+    dplyr::mutate(
+      estimate = as.numeric(estimate),
+      year = as.numeric(year)
+    ) |>
+    dplyr::rename(spawning_biomass = estimate) |>
+    dplyr::select(-c(module_name, label))
 
-  # Merge recruitment and spawning biomass data
-  sr <- dplyr::left_join(sb, recruitment)
+  # merge DF
+  sr <- dplyr::full_join(sb, rec) |>
+    dplyr::filter(year <= end_year)
 
   # Plot
-  # final <- 
-  plot_timeseries(
-    dat = sr,
-    x = "spawning_biomass",
-    y = "recruitment",
-    geom = "point",
-    xlab = paste("Spawning Biomass (", spawning_biomass_label, ")", sep = ""),
-    ylab = paste("Recruitment (", recruitment_label, ")", sep = "")
-  ) +
-  theme_noaa()
-  
-  # Make RDA
-  if (make_rda) {
-    create_rda(
-      object = final,
-      topic_label = ifelse(relative, "relative.spawning.biomass", "spawning.biomass"),
-      fig_or_table = "figure",
-      dat = rp_dat,
+  plt <- ggplot2::ggplot(data = sr) +
+    ggplot2::geom_point(ggplot2::aes(x = spawning_biomass, y = recruitment / 1000)) +
+    ggplot2::labs(
+      x = glue::glue("Spawning Biomass ({spawning_biomass_label})"),
+      y = glue::glue("Recruitment ({recruitment_label})")
+    ) +
+    ggplot2::theme(legend.position = "none")
+
+  final <- suppressWarnings(add_theme(plt))
+
+  # export figure to rda if argument = T
+  if (make_rda == TRUE) {
+    # run write_captions.R if its output doesn't exist
+    if (!file.exists(
+      fs::path(getwd(), "captions_alt_text.csv")
+    )
+    ) {
+      stockplotr::write_captions(
+        dat = dat,
+        dir = figures_dir,
+        year = end_year
+      )
+    }
+
+    # add more key quantities included as arguments in this fxn
+    add_more_key_quants(
+      topic = topic_label,
+      fig_or_table = fig_or_table,
       dir = figures_dir,
-      ref_line = ref_line,
-      ref_point = ref_line, # need to remove this I think
-      scale_amount = scale_amount,
-      unit_label = unit_label
+      end_year = end_year,
+      sr_ssb_units = spawning_biomass_label,
+      sr_recruitment_units = recruitment_label,
+      units = NULL
+    )
+
+    # extract this plot's caption and alt text
+    caps_alttext <- extract_caps_alttext(
+      topic_label = topic_label,
+      fig_or_table = fig_or_table,
+      dir = figures_dir
+    )
+
+    export_rda(
+      object = final,
+      caps_alttext = caps_alttext,
+      figures_tables_dir = figures_dir,
+      topic_label = topic_label,
+      fig_or_table = fig_or_table
     )
   }
-
-  # Export final plot
   final
 }
