be25f6f6e3cf9ccda73825769659bac71e459435
Schiano-NOAA <samantha.schiano@noaa.gov>
2025-08-18 17:56:39 -0400

    update sr documentation and add argument

--- R/plot_spawn_recruitment.R
+++ R/plot_spawn_recruitment.R
@@ -1,10 +1,16 @@
-#' Plot Spawn-Recruit Curve
+#' Plot Spawn-Recruit Relationship
 #'
-#' @inheritParams plot_recruitment
+#' @inheritParams plot_spawning_biomass
+#' @param interactive Indicate whether the environment the
+#' plot is being made in is interactive. By default, this
+#' is set to false. If true, dependent on your data, a
+#' option menu will pop-up.
 #' @param spawning_biomass_label Units for spawning biomass
 #' @param recruitment_label units for recruitment
 #'
-#' @return Plot spawning recruitment relationship from a standardized output file originating from \link[asar]{convert_output}
+#' @return Plot spawn recruitment relationship from a 
+#' standardized output file originating from 
+#' \link[asar]{convert_output}
 #' @export
 #'
 #' @examples
@@ -13,9 +19,9 @@
 #'
 #' plot_spawn_recruitment(
 #'   dat,
+#'   interactive = FALSE,
 #'   spawning_biomass_label = "sb label",
 #'   recruitment_label = "rec label",
-#'   end_year = 2024,
 #'   make_rda = TRUE,
 #'   figures_dir = getwd()
 #' )
@@ -24,27 +30,38 @@ plot_spawn_recruitment <- function(
     dat,
     spawning_biomass_label = "mt",
     recruitment_label = "mt",
-    end_year = format(Sys.Date(), "%Y"),
+    interactive = FALSE,
     make_rda = FALSE,
     figures_dir = getwd()) {
   # Extract recruitment
-  rec <- dat |>
-    dplyr::filter(
-      label == "recruitment",
-      module_name == "TIME_SERIES" | module_name == "t.series",
-      !is.na(year),
-      is.na(fleet) | length(unique(fleet)) <= 1,
-      is.na(sex) | length(unique(sex)) <= 1,
-      is.na(area) | length(unique(area)) <= 1,
-      is.na(growth_pattern) | length(unique(growth_pattern)) <= 1,
-      !year %in% year_exclusions
-    ) |> # SS3 and BAM target module names
-    dplyr::mutate(
-      estimate = as.numeric(estimate),
-      year = as.numeric(year)
-    ) |>
-    dplyr::rename(recruitment = estimate) |>
-    dplyr::select(-c(module_name, label))
+  recruitment <- prepare_data(
+    dat = dat,
+    label_name = "recruitment",
+    geom = "point",
+    interactive = interactive
+  ) 
+  if (length(unique(recruitment$label)) > 1) {
+    recruitment <- recruitment |>
+      tidyr::pivot_wider(
+      id_cols = c(year, model, group_var, estimate_lower, estimate_upper),
+      names_from = label,
+      values_from = estimate
+    )
+  } else {
+    recruitment <- recruitment |>
+    dplyr::rename(predicted_recruitment = estimate) |>
+    dplyr::select(-c(label))
+  }
+  
+  # Extract spawning biomass
+  sb <- prepare_data(
+    dat = dat,
+    label_name = "spawning biomass",
+    geom = "point",
+    interactive = interactive
+  ) |>
+  dplyr::rename(spawning_biomass = estimate) |>
+  dplyr::select(-c(label))
 
   stryr <- min(rec$year)
 
