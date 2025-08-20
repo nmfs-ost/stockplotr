52625f706705bd1d9ab45d5b41d42c2069f5c5c8
Sophie Breitbart <sophie.breitbart@noaa.gov>
2025-08-20 08:58:08 -0400

    Apply viridis palette to figures (#135)
    * remove exp_all_figs_tables() and add theme_noaa() to exports
    
    * Update theme_noaa() to apply viridis palette to figures; update documentation

---
+++ R/theme_noaa.R
@@ -0,0 +1,47 @@
+#' Add NOAA theming to ggplot2 object. The palette is from the 'viridis' package, which contains palettes distinguishable by those with color vision deficiency.
+#' 
+#' @param discrete Variable indicating whether the color or fill aesthetic is discrete. Default is TRUE.
+#' @param ... Arguments passed to `ggplot2::theme()`.
+#'
+#' @returns A list applying a theme to a ggplot2 object.
+#' @export
+#'
+#' @examples
+#' ggplot2::ggplot(data = OrchardSprays,
+#'                 ggplot2::aes(x = rowpos, 
+#'                              y = decrease,
+#'                              color = treatment)) +
+#'   ggplot2::geom_point() +
+#'   theme_noaa()
+theme_noaa <- function(discrete = TRUE,
+                       ...) {
+  theme <- list(
+    # might not need the ggplot2 dependencies if make this pkg an extension
+    ggplot2::theme(
+      plot.background = ggplot2::element_rect(fill = "transparent"),
+      panel.background = ggplot2::element_rect(fill = "transparent"),
+      panel.grid = ggplot2::element_blank(),
+      panel.border = ggplot2::element_rect(colour = "black", fill = NA, linewidth = 0.5),
+      complete = TRUE
+    )) 
+    
+    if (discrete){
+      list(theme,
+           ggplot2::scale_color_viridis_d(option = "mako",
+                              begin = 0.1,
+                              end = 0.85),
+           ggplot2::scale_fill_viridis_d(option = "mako",
+                             begin = 0.1,
+                             end = 0.85)
+           )
+    } else {
+      list(theme,
+           ggplot2::scale_color_viridis_c(option = "mako",
+                                          begin = 0.1,
+                                          end = 0.85),
+           ggplot2::scale_fill_viridis_c(option = "mako",
+                                         begin = 0.1,
+                                         end = 0.85)
+           )
+    }
+}
