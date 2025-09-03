ba41124388552382949bb5ac8310642cbfa085ee
Sophie Breitbart <sophie.breitbart@noaa.gov>
2025-09-03 13:13:44 -0400

    Revamped b plot (#136)
    * add URL and BugReports links to DESCRIPTION, the only non-outdated changes from https://github.com/nmfs-ost/stockplotr/pull/86#issuecomment-3212161587
    
    * add empty line to end of DESCRIPTION
    
    * Update documentation
    
    * Update required R version in DESCRIPTION
    
    * Update plot_biomass() params, function, documentation
    
    * Update plot_biomass test so that it tests when relative = TRUE
    
    * change interative default to TRUE
    
    ---------
    
    Co-authored-by: Schiano-NOAA <samantha.schiano@noaa.gov>

--- tests/testthat/test-plot_biomass.R
+++ tests/testthat/test-plot_biomass.R
@@ -4,18 +4,43 @@ load(file.path(
   "std_output.rda"
 ))
 
+# Make another sample dataset for testing relative
+n <- 448
+
+sim_df <- data.frame(
+  label = "biomass",
+  estimate = rlnorm(n, meanlog = 6.2, sdlog = 1.6),
+  module_name = "B.age",
+  time = NA,
+  season = NA,
+  subseason = NA,
+  fleet = NA,
+  sex = NA,
+  area = NA,
+  uncertainty_label = NA,
+  uncertainty = NA,
+  growth_pattern = NA,
+  age = sample(c(0,1,2,3,4,5,6), n, replace = TRUE),
+  era = sample(c("time", "fore"), n, replace = TRUE),
+  year = as.numeric(sample(2000:2024, n, replace = TRUE)),
+  nsim = as.numeric(1:n)) |>
+  dplyr::full_join(data.frame(label = "biomass_msy",
+                              estimate = 30000))
+
+
+
 test_that("plot_biomass generates plots without errors", {
   # expect error-free plot with minimal arguments
   expect_no_error(
     plot_biomass(out_new)
   )
-
+  
   # TODO: Update test
   # expect plot with warning message if ref_point not indicated
   # expect_message(
   #   plot_biomass(out_new)
   # )
-
+  
   # expect error-free plot with many arguments
   expect_no_error(
     plot_biomass(
@@ -29,30 +54,24 @@ test_that("plot_biomass generates plots without errors", {
       figures_dir = getwd()
     )
   )
-
+  
   # expect error-free plot when setting relative to T
   expect_no_error(
     plot_biomass(
-      out_new,
-      ref_point = 18000,
-      unit_label = "metric tons",
-      scale_amount = 1,
-      end_year = 2024,
-      relative = TRUE,
-      make_rda = FALSE,
-      figures_dir = getwd()
+      sim_df,
+      unit_label = "mt",
+      scale_amount = 10,
+      relative = TRUE
     )
   )
-
+  
   # expect ggplot object is returned
   expect_s3_class(
     plot_biomass(
       out_new,
-      ref_point = 18000,
       unit_label = "metric tons",
       scale_amount = 1,
       end_year = 2024,
-      relative = TRUE,
       make_rda = FALSE,
       figures_dir = getwd()
     ),
@@ -91,28 +110,14 @@ test_that("rda file made when indicated", {
     out_new,
     figures_dir = getwd(),
     make_rda = TRUE,
-    end_year = 2023,
-    ref_point = 18000
+    end_year = 2023
   )
-
+  
   # expect that both figures dir and the biomass_figure.rda file exist
   expect_true(dir.exists(fs::path(getwd(), "figures")))
   expect_true(file.exists(fs::path(getwd(), "figures", "biomass_figure.rda")))
-
+  
   # erase temporary testing files
   file.remove(fs::path(getwd(), "captions_alt_text.csv"))
   unlink(fs::path(getwd(), "figures"), recursive = T)
-})
-
-test_that("plot_biomass generates error with future end_year", {
-  # expect error
-  expect_error(
-    plot_biomass(
-      out_new,
-      figures_dir = getwd(),
-      make_rda = TRUE,
-      end_year = 2035,
-      ref_point = 18000
-    )
-  )
 })
