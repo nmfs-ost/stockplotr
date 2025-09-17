ddc374e50e23e819e5a749186d0047ecaa592ce0
Schiano-NOAA <samantha.schiano@noaa.gov>
2025-09-17 17:06:19 -0400

    adjust to all instead of all_of for check. Setting now so plot does not fail when the group doesn't exist

--- R/plot_spawning_biomass.R
+++ R/plot_spawning_biomass.R
@@ -144,8 +144,8 @@ plot_spawning_biomass <- function(
     geom = geom,
     ylab = spawning_biomass_label,
     group = group,
-    facet = facet,
-    ...
+    facet = facet#,
+    # ...
   )
   # Add reference line
   # getting data set - an ifelse statement in the fxn wasn't working
