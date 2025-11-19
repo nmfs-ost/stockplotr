e0fad1db15129bc8b2c8f097b035ea1ff4b40a6d
sbreitbart-NOAA <sophie.breitbart@noaa.gov>
2025-11-19 17:22:49 -0500

    Update create_table() to fix bugs producing latex-based table from data frame

--- R/utils_table.R
+++ R/utils_table.R
@@ -6,26 +6,35 @@ create_table <- function(data) {
   ncols <- ncol(data)
   column_names <- paste(colnames(data), collapse = " & ")
   latex_format_data <- paste(
-    column_names, "\n"
+    column_names, "\\\\", "\n"
   )
+  
+  # c signifies all cols will be centered
+  alignment <- strrep("c", ncols)
+  
   for (i in 1:nrow(data)) {
     row_data <- stringr::str_replace_all(
       paste(data[i,], collapse = " & "),
       "NA",
       "-")
+    
+    # ends up adding a space and two forward slashes to the end of each line
+    row_data_with_linebreak <- paste0(row_data, " \\\\")
+    
     latex_format_data <- paste(
       latex_format_data,
-      row_data, "\n",
+      row_data_with_linebreak, "\n",
+   #   row_data, "\n",
       sep = "",
       collapse = "\n"
     )
   }
   
   table <- paste0(
-    "\\begin{document}\n",
-    "\\begin{tabular}{lr}\n",
+   # "\\begin{document}\n",
+    "\\begin{tabular}{", alignment, "}\n",
     # multicolumn needs to = number of columns in the data set
-    "\\multicolumn{", ncols, "}{c}{Example}\n",# example is the first header - not sure we want bc it's a merged cell
+    "\\multicolumn{", ncols, "}{c}{Example} \\\\ \n",# example is the first header - not sure we want bc it's a merged cell
     # headers and values separated by '&' ending with 2 trailing forward slashes
     latex_format_data,
     "\\end{tabular}\n",
