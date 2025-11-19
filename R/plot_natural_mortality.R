030f48c10e3aff8e920a462fa96d56e5c4cd8b86
Schiano-NOAA <samantha.schiano@noaa.gov>
2025-11-19 07:56:51 -0500

    adjust M plot for errors with grouping

--- R/plot_natural_mortality.R
+++ R/plot_natural_mortality.R
@@ -30,7 +30,8 @@ plot_natural_mortality <- function(
   # -add to exp_all_figs_tables
   
   # Extract natural mortality
-  if (is.null(group)) group <- "age"
+  # if (is.null(group)) group <- "age"
+  
   prepared_data <- filter_data(
     dat,
     label_name = "natural_mortality",
@@ -52,13 +53,36 @@ plot_natural_mortality <- function(
   # STOP if there are no ages -- indicating this is a single M and would not be plotted
   if (all(is.na(prepared_data$age))) cli::cli_abort("Natural mortality by age not found.")
   
+  # Alert if group = "none"
+  if (!is.null(group) && group == "none") {
+    cli::cli_alert_warning("Setting group to 'none' is not functional for this plot. Setting group to NULL.")
+    group <- NULL
+  }
   # This process overrides grouping if it is inaccurate
-  processing <- process_data(prepared_data, group, facet)
-  # TODO: remove variable from process_data fxn output
-  # variable <- processing[[1]]
+  processing <- process_data(
+    dat = prepared_data,
+    group = "age",
+    facet = c(group, facet)
+  )
+
   processed_data <- processing[[1]]
-  group <- processing[[2]]
   if (!is.null(processing[[3]])) facet <- processing[[3]]
+  # make sure group is the one input by user
+  if (!is.null(group)) {
+    group <- group
+    processed_data <- processed_data |>
+      dplyr::mutate(group_var = .data[[group]])
+    # Remove grouping from facet bc included when run through process_data
+    facet <- facet[grepl(group, facet)]
+  }
+  # Move first facet into group if group is NULL
+  if (is.null(group) & length(facet) > 0) {
+    group <- facet[1]
+    facet <- facet[-1]
+    processed_data <- processed_data |>
+      dplyr::mutate(group_var = .data[[group]])
+  }
+  
   
   plt <- plot_timeseries(
     dat = processed_data |> dplyr::mutate(age = as.numeric(age)),
