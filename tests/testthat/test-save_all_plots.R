01f3705281b3a6e0f46c4185356c9455e6c96ccd
sbreitbart-NOAA <sophie.breitbart@noaa.gov>
2025-12-17 12:15:42 -0500

    Update save_all_plots() test

--- tests/testthat/test-save_all_plots.R
+++ tests/testthat/test-save_all_plots.R
@@ -23,71 +23,24 @@ test_that("save_all_plots works when all figures/tables are plotted", {
   # expect that the figures are all created with expected names
   fig_base_temp_files <- c(
     "biomass_figure.rda",
-    "pop.baa_figure.rda",
-    "catch_figure.rda",
+    "CPUE.indices_figure.rda",
+    "fishing.mortality_figure.rda",
     "landings_figure.rda",
-    "pop.caa_figure.rda", # not exporting in fxn for some reason
-    "pop.naa_figure.rda",
-    "recruitment.deviations_figure.rda",
-    "recruitment_figure.rda",
-    "spawning.biomass_figure.rda"
-  )
-  expect_equal(
-    list.files(fs::path(getwd(), "figures")),
-    fig_base_temp_files
-  )
-
-  # expect that the figures are all created with expected names
-  # tab_base_temp_files <- c(
-  #   "bnc_table.rda",
-  #   "indices.abundance_table.rda",
-  #   "landings_table.rda"
-  # )
-  # expect_equal(
-  #   list.files(fs::path(getwd(), "tables")),
-  #   tab_base_temp_files
-  # )
-
-  # erase temporary testing files
-  file.remove(fs::path(getwd(), "captions_alt_text.csv"))
-  unlink(fs::path(getwd(), "figures"), recursive = T)
-  # unlink(fs::path(getwd(), "tables"), recursive = T)
-})
-
-test_that("save_all_plots works when some figures/tables are not plotted", {
-  # plot all figs/tables except for plot_biomass
-  stockplotr::save_all_plots(out_new,
-    # add an unreal ref_line so plot_biomass doesn't work
-    ref_line = "not_a_real_ref_line",
-    ref_line_sb = "target",
-    # indices_unit_label = "CPUE",
-    figures_tables_dir = getwd(),
-    interactive = FALSE
-  )
-
-  # expect that the figures and tables dirs exist
-  expect_true(dir.exists(fs::path(getwd(), "figures")))
-  # expect_true(dir.exists(fs::path(getwd(), "tables")))
-
-  # expect that the figures are all created with expected names
-  # except for biomass_figure
-  fig_base_temp_files <- c(
-    "biomass_figure.rda",
+    "natural.mortality_figure.rda",
     "pop.baa_figure.rda",
-    "catch_figure.rda",
-    "landings_figure.rda",
     "pop.caa_figure.rda",
     "pop.naa_figure.rda",
-    "recruitment.deviations_figure.rda",
     "recruitment_figure.rda",
-    "spawning.biomass_figure.rda"
+    "recruitment.deviations_figure.rda",
+    "spawning.biomass_figure.rda",
+    "sr_figure.rda"
   )
   expect_equal(
-    list.files(fs::path(getwd(), "figures")),
-    fig_base_temp_files
+    sort(list.files(fs::path(getwd(), "figures"))),
+    sort(fig_base_temp_files)
   )
 
-  # expect that the figures are all created with expected names
+  # expect that the tables are all created with expected names
   # tab_base_temp_files <- c(
   #   "bnc_table.rda",
   #   "indices.abundance_table.rda",
@@ -97,6 +50,7 @@ test_that("save_all_plots works when some figures/tables are not plotted", {
   #   list.files(fs::path(getwd(), "tables")),
   #   tab_base_temp_files
   # )
+
   # erase temporary testing files
   file.remove(fs::path(getwd(), "captions_alt_text.csv"))
   unlink(fs::path(getwd(), "figures"), recursive = T)
