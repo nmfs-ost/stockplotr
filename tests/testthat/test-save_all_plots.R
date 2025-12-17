62d4f8864daee63f9dd7f1febc1de459d4d6b9e0
sbreitbart-NOAA <sophie.breitbart@noaa.gov>
2025-12-17 12:15:42 -0500

    Update save_all_plots() test

--- tests/testthat/test-save_all_plots.R
+++ tests/testthat/test-save_all_plots.R
@@ -79,20 +79,19 @@ test_that("save_all_plots works when some figures/tables are not plotted", {
   fig_base_temp_files <- c(
     "biomass_figure.rda",
     "pop.baa_figure.rda",
-    "catch_figure.rda",
-    "landings_figure.rda",
     "pop.caa_figure.rda",
     "pop.naa_figure.rda",
-    "recruitment.deviations_figure.rda",
     "recruitment_figure.rda",
-    "spawning.biomass_figure.rda"
+    "recruitment.deviations_figure.rda",
+    "spawning.biomass_figure.rda",
+    "sr_figure.rda"
   )
   expect_equal(
-    list.files(fs::path(getwd(), "figures")),
-    fig_base_temp_files
+    sort(list.files(fs::path(getwd(), "figures"))),
+    sort(fig_base_temp_files)
   )
 
-  # expect that the figures are all created with expected names
+  # expect that the tables are all created with expected names
   # tab_base_temp_files <- c(
   #   "bnc_table.rda",
   #   "indices.abundance_table.rda",
@@ -102,6 +101,7 @@ test_that("save_all_plots works when some figures/tables are not plotted", {
   #   list.files(fs::path(getwd(), "tables")),
   #   tab_base_temp_files
   # )
+
   # erase temporary testing files
   file.remove(fs::path(getwd(), "captions_alt_text.csv"))
   unlink(fs::path(getwd(), "figures"), recursive = T)
