7c0a83c6e4ae28746df6f2fcb066a98db1c211ff
Sam Schiano <125507018+Schiano-NOAA@users.noreply.github.com>
2026-01-23 10:19:19 -0500

    Format tables to merge error to estimates (#181)
    * begin to fix landings table where the error and value labels are combined
    
    * finish merge_error fxn and incorporate into table_landings
    
    * add documentation to merge_error fxn and remove leftover testing inside fxn
    
    * remove statement that changes list to df when only one model
    
    * change landings to label in merge_error to make more general for other tables
    
    * add condition when only 2 cols and uncert is not there
    
    * move merge_error into table utils
    
    * update documentation
    
    * add flextable back in since it's referenced in add_theme function

--- tests/testthat/test-table_landings.R
+++ tests/testthat/test-table_landings.R
@@ -1,67 +1,81 @@
-# # load sample dataset
-# load(file.path(
-#   "fixtures", "ss3_models_converted", "Hake_2018",
-#   "std_output.rda"
-# ))
+# load sample dataset
+load(file.path(
+  "fixtures", "ss3_models_converted", "Hake_2018",
+  "std_output.rda"
+))
 
-# test_that("table_landings generates plots without errors", {
-#   # expect error-free plot with minimal arguments
-#   expect_no_error(
-#     stockplotr::table_landings(out_new)
-#   )
+test_that("table_landings generates plots without errors", {
+  # expect error-free plot with minimal arguments
+  expect_no_error(
+    table_landings(
+      out_new,
+      interactive = FALSE,
+      module = "CATCH"
+      )
+  )
 
-#   # expect error-free plot with many arguments
-#   expect_no_error(
-#     stockplotr::table_landings(
-#       out_new,
-#       unit_label = "metric tons",
-#       end_year = 2024,
-#       make_rda = FALSE,
-#       tables_dir = getwd()
-#     )
-#   )
+  # expect error-free plot with many arguments
+  expect_no_error(
+    table_landings(
+      dat = out_new,
+      unit_label = "mt",
+      interactive = FALSE,
+      module = "CATCH",
+      make_rda = FALSE,
+      tables_dir = getwd()
+    )
+  )
 
 
-#   # expect flextable object is returned
-#   expect_s3_class(
-#     stockplotr::table_landings(
-#       out_new,
-#       unit_label = "metric tons",
-#       make_rda = FALSE,
-#       tables_dir = getwd()
-#     ),
-#     "flextable"
-#   )
-# })
+  # expect gt object is returned
+  # adjust this test to work for multiple output tables
+  # expect_s3_class(
+  #   table_landings(
+  #     dat = out_new,
+  #     unit_label = "mt",
+  #     era = NULL,
+  #     interactive = FALSE,
+  #     module = "CATCH",
+  #     make_rda = FALSE,
+  #     tables_dir = getwd()
+  #   ),
+  #   "gt_tbl"
+  # )
+})
 
 # test_that("rda file made when indicated", {
 #   # export rda
 #   table_landings(
-#     out_new,
-#     unit_label = "metric tons",
-#     end_year = 2024,
+#     dat = out_new,
+#     unit_label = "mt",
+#     era = NULL,
+#     interactive = FALSE,
+#     module = "CATCH",
 #     make_rda = TRUE,
 #     tables_dir = getwd()
 #   )
-
+# 
 #   # expect that both tables dir and the landings_table.rda file exist
 #   expect_true(dir.exists(fs::path(getwd(), "tables")))
 #   expect_true(file.exists(fs::path(getwd(), "tables", "landings_table.rda")))
-
+# 
 #   # erase temporary testing files
 #   file.remove(fs::path(getwd(), "captions_alt_text.csv"))
 #   unlink(fs::path(getwd(), "tables"), recursive = T)
 # })
 
-# test_that("table_landings generates error with future end_year", {
-#   # expect error
-#   expect_error(
-#     stockplotr::table_landings(
-#       out_new,
-#       unit_label = "metric tons",
-#       end_year = 2055,
-#       make_rda = FALSE,
-#       tables_dir = getwd()
-#     )
-#   )
-# })
+test_that("table_landings generates error with incorrect module", {
+  # expect error
+  # Need to test this -- not exactly the right test/result
+  expect_error(
+    table_landings(
+      dat = out_new,
+      unit_label = "mt",
+      era = NULL,
+      interactive = FALSE,
+      module = "SPR_SERIES",
+      make_rda = FALSE,
+      tables_dir = getwd()
+    )
+  )
+})
