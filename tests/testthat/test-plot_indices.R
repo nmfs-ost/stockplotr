39e92624b328c5c4e83c753daf3341709b0ca918
sbreitbart-NOAA <sophie.breitbart@noaa.gov>
2026-03-02 13:40:03 -0500

    add tests for landings and indices plots

--- tests/testthat/test-plot_indices.R
+++ tests/testthat/test-plot_indices.R
@@ -4,32 +4,31 @@ load(file.path(
   "std_output.rda"
 ))
 
-test_that("plot_landings generates plots without errors", {
+test_that("plot_indices generates plots without errors", {
   # expect error-free plot with minimal arguments
   expect_no_error(
-    plot_landings(out_new)
+    plot_indices(out_new)
   )
 
 
   # expect ggplot object is returned
   expect_s3_class(
-    plot_landings(out_new),
+    plot_indices(out_new),
     "gg"
   )
 })
 
 test_that("rda file made when indicated", {
   # export rda
-  plot_landings(out_new,
-    end_year = 2024,
+  plot_indices(out_new,
     make_rda = TRUE,
     unit_label = "metric tons",
     figures_dir = getwd()
   )
 
-  # expect that both figures dir and the landings_figure.rda file exist
+  # expect that both figures dir and the indices_figure.rda file exist
   expect_true(dir.exists(fs::path(getwd(), "figures")))
-  expect_true(file.exists(fs::path(getwd(), "figures", "landings_figure.rda")))
+  expect_true(file.exists(fs::path(getwd(), "figures", "indices_figure.rda")))
 
   # erase temporary testing files
   file.remove(fs::path(getwd(), "captions_alt_text.csv"))
