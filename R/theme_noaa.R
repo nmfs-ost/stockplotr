c73b3539c5e92924a27d1fe8ee1a0e5492abe8b4
sbreitbart-NOAA <sophie.breitbart@noaa.gov>
2026-03-04 10:10:32 -0500

    Update theme_noaa() to use ggplot2 v4.0 syntax and allow global theming, prompted by https://github.com/nmfs-ost/stockplotr/issues/143; update DESCRIPTION

--- R/theme_noaa.R
+++ R/theme_noaa.R
@@ -17,48 +17,47 @@
 #' ) +
 #'   ggplot2::geom_point() +
 #'   theme_noaa()
+#'\dontrun{
+#'  ggplot2::theme_set(stockplotr::theme_noaa())
+#'}
 theme_noaa <- function(discrete = TRUE,
                        ...) {
-  theme <- list(
-    # might not need the ggplot2 dependencies if make this pkg an extension
+  
+  if (utils::packageVersion("ggplot2") < "4.0.0") {
+    rlang::warn(
+      message = paste0("Your `ggplot2` version is ", utils::packageVersion("ggplot2"), ", which is older than the version required to use `theme_noaa()` (4.0.0). Please update your `ggplot2` package."),
+      .frequency = "once",
+      .frequency_id = "ggplot2_version_warning"
+    )
+  }
+  
+  theme <- ggplot2::theme_bw() +
     ggplot2::theme(
       plot.background = ggplot2::element_rect(fill = "transparent"),
       panel.background = ggplot2::element_rect(fill = "transparent"),
       panel.grid = ggplot2::element_blank(),
       panel.border = ggplot2::element_rect(colour = "black", fill = NA, linewidth = 0.5),
-      complete = TRUE,
       text = ggplot2::element_text(size = 14),
-      plot.margin = ggplot2::margin(0.5, 1, 0.5, 0.5, "cm")
-    )
+      plot.margin = ggplot2::margin(0.5, 1, 0.5, 0.5, "cm"),
+      ...
   )
 
+  # create main palette
+  pal <- function(n) viridisLite::mako(n, begin = 0.1, end = 0.85)
+  
   if (discrete) {
-    list(
-      theme,
-      ggplot2::scale_color_viridis_d(
-        option = "mako",
-        begin = 0.1,
-        end = 0.85
-      ),
-      ggplot2::scale_fill_viridis_d(
-        option = "mako",
-        begin = 0.1,
-        end = 0.85
-      )
-    )
-  } else {
-    list(
-      theme,
-      ggplot2::scale_color_viridis_c(
-        option = "mako",
-        begin = 0.1,
-        end = 0.85
-      ),
-      ggplot2::scale_fill_viridis_c(
-        option = "mako",
-        begin = 0.1,
-        end = 0.85
-      )
-    )
-  }
+    theme <- theme +
+      ggplot2::theme(
+       palette.colour.discrete = pal,
+       palette.fill.discrete = pal
+       )
+    } else {
+      theme <- theme +
+        ggplot2::theme(
+          palette.colour.continuous = pal,
+          palette.fill.continuous = pal
+        )
+    }
+  
+  theme
 }
