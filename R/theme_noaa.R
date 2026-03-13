1fd884eade2654db0d31b76a63f2a8d5ae3daa0b
Sophie Breitbart <sophie.breitbart@noaa.gov>
2026-03-13 10:14:04 -0400

    Remove unnecessary dependencies from theme_noaa() (#206)
    * Remove viridislite pkg from theme_noaa() as per https://github.com/nmfs-ost/stockplotr/issues/204#issuecomment-4048764333
    
    * Remove rlang pkg from theme_noaa()
    
    * Remove notes

--- R/theme_noaa.R
+++ R/theme_noaa.R
@@ -17,20 +17,39 @@
 #' ) +
 #'   ggplot2::geom_point() +
 #'   theme_noaa()
-#' \dontrun{
-#' ggplot2::theme_set(stockplotr::theme_noaa())
-#' }
+#' 
+#' ggplot2::ggplot(
+#'   data = OrchardSprays,
+#'   ggplot2::aes(
+#'     x = rowpos,
+#'     y = decrease,
+#'     color = decrease
+#'   )
+#' ) +
+#'   ggplot2::geom_point() +
+#'   theme_noaa(discrete = FALSE)
+#'\dontrun{
+#'  ggplot2::theme_set(stockplotr::theme_noaa())
+#'}
 theme_noaa <- function(discrete = TRUE,
                        ...) {
   if (utils::packageVersion("ggplot2") < "4.0.0") {
-    rlang::warn(
-      message = paste0("Your `ggplot2` version is ", utils::packageVersion("ggplot2"), ", which is older than the version required to use `theme_noaa()` (4.0.0). Please update your `ggplot2` package."),
+    cli::cli_warn(
+      "Your {.pkg ggplot2} version is {.val {utils::packageVersion('ggplot2')}}, 
+     which is older than the version required to use {.fn theme_noaa} (4.0.0). 
+     Please update your {.pkg ggplot2} package.",
       .frequency = "once",
       .frequency_id = "ggplot2_version_warning"
     )
   }
-
-  theme <- ggplot2::theme_bw() +
+  
+  mako_pal_discrete <- scales::pal_viridis(begin = 0.1, end = 0.85, option = "G")
+  
+  mako_pal_continuous <- scales::gradient_n_pal(
+    scales::pal_viridis(begin = 0.1, end = 0.85, option = "G")(6)
+  )
+  
+  thm <- ggplot2::theme_bw() + 
     ggplot2::theme(
       plot.background = ggplot2::element_rect(fill = "transparent"),
       panel.background = ggplot2::element_rect(fill = "transparent"),
@@ -40,23 +59,17 @@ theme_noaa <- function(discrete = TRUE,
       plot.margin = ggplot2::margin(0.5, 1, 0.5, 0.5, "cm"),
       ...
     )
-
-  # create main palette
-  pal <- function(n) viridisLite::mako(n, begin = 0.1, end = 0.85)
-
+  
   if (discrete) {
-    theme <- theme +
-      ggplot2::theme(
-        palette.colour.discrete = pal,
-        palette.fill.discrete = pal
-      )
+    thm <- thm + ggplot2::theme(
+      palette.colour.discrete = mako_pal_discrete,
+      palette.fill.discrete   = mako_pal_discrete
+    )
   } else {
-    theme <- theme +
-      ggplot2::theme(
-        palette.colour.continuous = pal,
-        palette.fill.continuous = pal
-      )
+    thm <- thm + ggplot2::theme(
+      palette.colour.continuous = mako_pal_continuous,
+      palette.fill.continuous   = mako_pal_continuous
+    )
   }
-
-  theme
+  thm
 }
