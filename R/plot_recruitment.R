70f2e8de881aeae81f354d79dfb08973aa45cfb7
Sam Schiano <125507018+Schiano-NOAA@users.noreply.github.com>
2026-04-01 14:27:58 -0400

    add in optional ggplot args

--- R/plot_recruitment.R
+++ R/plot_recruitment.R
@@ -35,7 +35,8 @@ plot_recruitment <- function(
   interactive = TRUE,
   module = NULL,
   make_rda = FALSE,
-  figures_dir = getwd()
+  figures_dir = getwd(),
+  ...
 ) {
   # TODO: Fix the unit label if scaling
   recruitment_label <- label_magnitude(
@@ -98,12 +99,12 @@ plot_recruitment <- function(
     dat = recruitment,
     x = "year",
     y = "predicted_recruitment",
-    color = "black",
+    # color = "black",
     geom = geom,
     xlab = "Year",
     ylab = recruitment_label,
     group = group,
-    facet = facet # ,
+    facet = facet #,
     # ...
   ) +
     theme_noaa()
