7346b8d09f3bb0aeb02b908ab205459013a44cb1
Sam Schiano <125507018+Schiano-NOAA@users.noreply.github.com>
2026-04-30 16:07:41 -0400

    remove outdated tests for sb plot

--- tests/testthat/test-plot_spawning_biomass.R
+++ tests/testthat/test-plot_spawning_biomass.R
@@ -25,17 +25,6 @@ test_that("plot_spawning_biomass generates plots without errors", {
     )
   )
 
-  # expect error-free plot when setting relative to T
-  expect_no_error(
-    plot_spawning_biomass(
-      out_new,
-      unit_label = "metric tons",
-      scale_amount = 1,
-      ref_line = "msy",
-      relative = TRUE
-    )
-  )
-
   # expect ggplot object is returned
   expect_s3_class(
     plot_spawning_biomass(
@@ -43,39 +32,11 @@ test_that("plot_spawning_biomass generates plots without errors", {
       unit_label = "metric tons",
       scale_amount = 1,
       ref_line = c("msy" = 2000000),
-      relative = TRUE
+      relative = FALSE
     ),
     "gg"
   )
 })
-
-# Test now mute bc will always plot like when reference point is present
-# test_that("plot_spawning_biomass plots contain reference point when indicated", {
-#   # expect plot with a reference point (horizontal line) contains 4 layers while
-#   # plot w/o ref pt contains only 3 layers
-
-#   # make sb plot with reference point
-#   sb_ref <- plot_spawning_biomass(out_new,
-#     ref_line = c("msy" = 1800099),
-#     module = "TIME_SERIES"
-#   )
-#   # extract number of layers (should be 4)
-#   sb_ref_layers <- sb_ref[["layers"]] |>
-#     length()
-
-#   # make sb plot without reference point
-#   sb_no_ref <- plot_spawning_biomass(
-#     out_new,
-#     module = "TIME_SERIES")
-#   # extract number of layers (should be 3)
-#   sb_no_ref_layers <- sb_no_ref[["layers"]] |>
-#     length()
-
-#   expect_equal(
-#     (sb_ref_layers - 1),
-#     sb_no_ref_layers
-#   )
-# })
 
 test_that("rda file made when indicated", {
   # export rda
@@ -99,17 +60,3 @@ test_that("rda file made when indicated", {
   unlink(fs::path(getwd(), "figures"), recursive = T)
 })
 
-# Test no longer applicable bc end_year is not an argument
-# test_that("plot_spawning_biomass generates error with future end_year", {
-#   # expect error
-#   expect_error(
-#     plot_spawning_biomass(
-#       out_new,
-#       unit_label = "metric tons",
-#       end_year = 2055,
-#       scale_amount = 1,
-#       ref_point = 100,
-#       relative = TRUE
-#     )
-#   )
-# })
