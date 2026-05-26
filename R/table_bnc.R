36a9a2283c45aab2bae8a8d75a26abf5340df389
sbreitbart-NOAA <sophie.breitbart@noaa.gov>
2026-05-26 17:00:34 -0400

    Add key quantities to bnc caption; remove unnecessary 'year' arg from create_rda(); start "revamping" bnc table to match structure and formatting of landings tbl

--- R/table_bnc.R
+++ R/table_bnc.R
@@ -1,61 +1,206 @@
-# #' Biomass, abundance, and catch time series table
-# #'
-# #' @inheritParams plot_recruitment
-# #' @param biomass_unit_label abbreviated units for biomass
-# #' @param catch_unit_label abbreviated units for catch
-# #' @param catch_unit_label abbreviated units for catch
-# #' @param sb_unit_label abbreviated units for spawning biomass
-# #' @param tables_dir The location of the folder containing the generated table
-# #' rda files ("tables") that will be created if the argument `make_rda` = TRUE.
-# #' Default is the working directory.
-# #'
-# #' @returns A table of biomass, abundance, catch, and spawning biomass through all years of
-# #' the assessment model output translated to a standard structure.
-# #' @details There are
-# #' options to return a [gt::gt()] object or export an rda object containing
-# #' a gt-based table, caption, and LaTeX-based table.
-# #' @seealso [convert_output()], [filter_data()], [process_table()], [export_kqs()], [insert_kqs()], [create_rda()]
-# #' @export
-# #'
-# #' @examples
-# #' \dontrun{
-# #' table_bnc(dat)
-# #'
-# #' table_bnc(dat,
-# #'   end_year = 2024,
-# #'   biomass_unit_label = "b label",
-# #'   catch_unit_label = "catch label",
-# #'   make_rda = TRUE,
-# #'   tables_dir = getwd()
-# #' )
-# #' }
-# table_bnc <- function(
-#     dat,
-#     end_year = format(Sys.Date(), "%Y"),
-#     biomass_unit_label = "mt",
-#     catch_unit_label = "mt",
-#     sb_unit_label = "mt",
-#     make_rda = FALSE,
-#     tables_dir = getwd()) {
+#' Biomass, abundance, and catch time series table
+#'
+#' @inheritParams plot_recruitment
+#' @param biomass_unit_label Abbreviated units for biomass
+#' Default: "mt"
+#' 
+#' @param catch_unit_label Abbreviated units for catch
+#' Default: "mt"
+#' 
+#' @param sb_unit_label Abbreviated units for spawning biomass
+#' Default: "mt"
+#' 
+#' @param group A string of a single column that groups the data.
+#'
+#' Set group = "none" to summarize data over all indexing values.
+#'
+#' Default: NULL
+#' Options: Including, but not limited to: "year", "area", "fleet", "sex", "none", NULL
+#' @param method A string describing the method of summarizing data when group
+#' is set to "none".
+#'
+#' Default: "sum"
+#'
+#' Options: "sum" or "mean"
+#' 
+#' @param label The label that will be chosen from the input file. If unspecified,
+#' the function will search the "label" column and use the first matching label
+#' in this ordered list: "landings_weight",  "landings_numbers", "landings_expected",
+#' "landings_predicted", "landings".
+#' 
+#' Default: NULL
+#'
+#' @param tables_dir The location of the folder containing the table
+#' rda files ("tables") that will be created if the argument `make_rda` = TRUE.
+#' Default: the working directory (`getwd()`)
+#'
+#' @returns A table of biomass, abundance, catch, and spawning biomass.
+#' @details There are
+#' options to return a [gt::gt()] object or export an rda object containing
+#' a gt-based table, caption, and LaTeX-based table.
+#' @seealso [convert_output()], [filter_data()], [process_table()], [export_kqs()], [insert_kqs()], [create_rda()]
+#' @export
+#'
+#' @examples
+#' table_bnc(stockplotr::example_data)
+#'
+#' table_bnc(stockplotr::example_data,
+#'   biomass_unit_label = "b label",
+#'   catch_unit_label = "catch label"
+#' )
+table_bnc <- function(
+    dat,
+    biomass_unit_label = "mt",
+    catch_unit_label = "mt",
+    sb_unit_label = "mt",
+    era = NULL,
+    interactive = TRUE,
+    group = NULL,
+    method = "sum",
+    module = NULL,
+    label = NULL,
+    make_rda = FALSE,
+    tables_dir = getwd()) {
+  
+  # TODO: do group and facet need to be uncommented and updated?
+  # Filter data for landings
+  prepared_data <- filter_data(
+    dat = dat,
+    label_name = "bnc",
+    geom = "line",
+    era = era,
+    module = module,
+    scale_amount = 1,
+    interactive = interactive
+  ) |>
+    dplyr::mutate(estimate = round(as.numeric(estimate), digits = 0)) |>
+    dplyr::mutate(uncertainty = round(as.numeric(uncertainty), digits = 2))
+  
+  # Add check if there is any data
+  if (nrow(prepared_data) == 0) {
+    cli::cli_abort("No bnc data found.")
+  }
+  
+  # get uncertainty label by model
+  uncert_lab <- prepared_data |>
+    dplyr::filter(!is.na(uncertainty_label)) |>
+    dplyr::group_by(model) |>
+    dplyr::reframe(unique_uncert = unique(uncertainty_label)) # changed to reframe -- may cause errors
+  uncert_lab <- stats::setNames(uncert_lab$unique_uncert, uncert_lab$model)
+  # if (length(unique(uncert_lab)) == 1) uncert_lab <- unique(uncert_lab) # might need this line
+  
+  # This needs to be adjusted when comparing different models and diff error
+  if (length(uncert_lab) > 1 & length(unique(uncert_lab)) == 1 | length(names(uncert_lab)) == 1) { # prepared_data$model
+    # cli::cli_alert_warning("More than one value for uncertainty exists: {uncert_lab}")
+    uncert_lab <- uncert_lab[[1]]
+    # cli::cli_alert_warning("The first value ({uncert_lab}) will be chosen.")
+  }
+  
+  if (is.na(uncert_lab)) uncert_lab <- "uncertainty"
+  
+  # get fleet names
+  # TODO: change from fleets to id_group AFTER the process data step and adjust throughout the table based on indexing
+  fleets <- unique(prepared_data$fleet) |>
+    # sort numerically even if fleets are 100% characters
+    stringr::str_sort(numeric = TRUE)
+  
+  # TODO: fix this so that fleet names aren't removed if, e.g., group = "fleet"
+  table_data_info <- process_table(
+    dat = prepared_data,
+    # group = group,
+    method = method,
+    label = label
+  )
+  table_data <- table_data_info[[1]]
+  indexed_vars <- table_data_info[[2]]
+  id_col_vals <- table_data_info[[3]]
+  
+  # id_group_vals <- sapply(id_cols, function(x) unique(prepared_data[[x]]), simplify = FALSE)
+  # TODO: add check if there is a landings column for every error column -- if not remove the error (can keep landings)
+  
+  # merge error and landings columns and rename
+  df_list <- merge_error(
+    table_data,
+    uncert_lab,
+    fleets,
+    label = "landings",
+    unit_label
+  )
+  
+  # transform dfs into tables
+  final <- lapply(df_list, function(df) {
+    df |>
+      gt::gt() |>
+      add_theme()
+  })
+  
+  # export figure to rda if argument = T
+  if (make_rda == TRUE) {
+    if (length(df_list) == 1) {
+      # Obtain relevant key quantities for captions/alt text
+      bnc.b.units <- biomass_unit_label
+      bnc.catch.units <- catch_unit_label
+      bnc.sb.units <- sb_unit_label
+      
+      # calculate & export key quantities
+      export_kqs(bnc.b.units,
+                 bnc.catch.units,
+                 bnc.sb.units)
+      
+      # Add key quantities to captions/alt text
+      insert_kqs(bnc.b.units,
+                 bnc.catch.units,
+                 bnc.sb.units)
+      
+      create_rda(
+        object = final$label,
+        # get name of function and remove "table_" from it
+        topic_label = gsub("table_", "", as.character(sys.call()[[1]])),
+        fig_or_table = "table",
+        dat = dat,
+        dir = tables_dir,
+        scale_amount = 1,
+        unit_label = biomass_unit_label,
+        table_df = final
+      )
+    }
+  } else {
+    cli::cli_alert_warning("Multiple tables cannot be exported at this time.")
+    cli::cli_alert_info("We are currently developing this feature.")
+  }
+  
+  # Send table(s) to viewer
+  if (!is.data.frame(table_data)) {
+    for (t in final) {
+      print(t)
+    }
+    # Return table list invisibly
+    return(invisible(final))
+  } else {
+    # Return finished table (when only one table)
+    return(final)
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 #   biomass_label <- glue::glue("Biomass ({biomass_unit_label})")
 #   catch_label <- glue::glue("Catch ({catch_unit_label})")
 #   sb_label <- glue::glue("Spawning biomass ({sb_unit_label})")
 # TODO: Update documentation to match formatting of other functions, with
 # lines for Default and Options
-#   # create plot-specific variables to use throughout fxn for naming and IDing
-#   topic_label <- "bnc"
-
-#   # identify output
-#   fig_or_table <- "table"
-
-#   # check year isn't past end_year if not projections plot
-#   check_year(
-#     end_year = end_year,
-#     fig_or_table = fig_or_table,
-#     topic = topic_label
-#   )
-
-#   dat <- dplyr::filter(dat, year <= end_year)
 
 #   biomass <- dat |>
 #     dplyr::filter(
