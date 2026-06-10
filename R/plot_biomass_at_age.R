2339cbb7d6768f6989554fb47ac50b0988392692
sbreitbart-NOAA <sophie.breitbart@noaa.gov>
2026-06-10 14:47:06 -0400

    [hotfix] Fix method for extracting plot/table name when exporting rda to allow for calling library when making plot

--- R/plot_biomass_at_age.R
+++ R/plot_biomass_at_age.R
@@ -142,7 +142,7 @@ plot_biomass_at_age <- function(
     create_rda(
       object = plot,
       # get name of function and remove "plot_" from it
-      topic_label = gsub("plot_", "", as.character(sys.call()[[1]])),
+      topic_label = gsub("plot_", "", tail(as.character(sys.call()[[1]]), n = 1)),
       fig_or_table = "figure",
       dat,
       unit_label = "mt"
