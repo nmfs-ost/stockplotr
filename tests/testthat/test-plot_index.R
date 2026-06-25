d8acba1d4c0f0ce14f3831b21c2caf0b025c3d5c
Sam Schiano <125507018+Schiano-NOAA@users.noreply.github.com>
2026-06-25 16:34:03 -0400

    update documentation and fix plot_index test

--- tests/testthat/test-plot_index.R
+++ tests/testthat/test-plot_index.R
@@ -1,20 +1,19 @@
-test_that("plot_indices generates plots without errors", {
+test_that("plot_index generates plots without errors", {
   # expect error-free plot with minimal arguments
   expect_no_error(
-    plot_indices(stockplotr::example_data)
+    plot_index(stockplotr::example_data)
   )
-
 
   # expect ggplot object is returned
   expect_s3_class(
-    plot_indices(stockplotr::example_data),
+    plot_index(stockplotr::example_data),
     "gg"
   )
 })
 
 test_that("rda file made when indicated", {
   # export rda
-  plot_indices(stockplotr::example_data,
+  plot_index(stockplotr::example_data,
     make_rda = TRUE,
     unit_label = "metric tons",
     figures_dir = getwd()
