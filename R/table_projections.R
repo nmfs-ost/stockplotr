ae2c2648b4d0de962ee1ea658abe1c0660d1c927
sbreitbart-NOAA <sophie.breitbart@noaa.gov>
2026-07-17 15:27:21 -0400

    Update table_projections()' documentation; update save_all_plots()' documentation and add table_projections() to it; update captions/alt text template; update table_projections()' test

--- R/table_projections.R
+++ R/table_projections.R
@@ -3,6 +3,17 @@
 #' Generates a table showing assumed and projected years of Catch, SB, and F.
 #'
 #' @inheritParams table_landings
+#' 
+#' @param unit_label Character vector. Abbreviated unit label for each quantity
+#' 
+#' Default: c("catch" = "mt", "spawning_biomass" = "mt", 
+#' "fishing_mortality" = "")
+#' 
+#' @param uncertainty_label Character vector. Abbreviated uncertainty label for
+#' each quantity
+#' 
+#' Default: c("catch" = "stddev", "spawning_biomass" = "stddev",
+#' "fishing_mortality" = "stddev")
 #'
 #' @return A formatted gt table object.
 #' @details We would like to thank Dan Hennen for sharing his projections table
@@ -16,7 +27,7 @@
 table_projections <- function(
     dat,
     unit_label = c("catch" = "mt", "spawning_biomass" = "mt", "fishing_mortality" = ""),
-    uncertainty = c("catch" = "stddev", "spawning_biomass" = "stddev", "fishing_mortality" = "stddev"),
+    uncertainty_label = c("catch" = "stddev", "spawning_biomass" = "stddev", "fishing_mortality" = "stddev"),
     interactive = TRUE,
     module = NULL,
     make_rda = FALSE,
@@ -44,12 +55,13 @@ table_projections <- function(
                                       digits = ifelse(x == "fishing_mortality", 5, 2))
       data <- processed_data[[1]]
       group <- processed_data[[2]]
+
       # merge data with uncertainty and unit labels
       merge_error(
         table_data = data,
         id_col_vals = group,
         unit_label = unit_label[grepl(x, names(unit_label))][[1]],
-        uncert_lab = uncertainty[grepl(x, names(uncertainty))][[1]]
+        uncert_lab = uncertainty_label[grepl(x, names(uncertainty_label))][[1]]
       )[[1]]
     }
   )
@@ -61,13 +73,24 @@ table_projections <- function(
   
   # export figure to rda if argument = T
   if (make_rda == TRUE) {
-      # No key quantities for captions/alt text since only values
-      # are units for catch, SB, and F, which are specified for
-      # the main figures/tables for each
-      # landings.units <- unit_label
+    # No key quantities for captions/alt text since only values
+    # are units for catch, SB, and F, which are specified for
+    # the main figures/tables for each
+    # So, export captions/alt text csv if absent
+    if (!file.exists(fs::path(getwd(), "captions_alt_text.csv"))) {
+      caps_alttext <- utils::read.csv(
+        system.file("resources", "captions_alt_text_template.csv", package = "stockplotr")
+      )
+      # export df with captions and alt text to csv
+      utils::write.csv(
+        x = caps_alttext,
+        file = fs::path(getwd(), "captions_alt_text.csv"),
+        row.names = FALSE
+      )    
+    }
      
       create_rda(
-        object = final$label,
+        object = final_table$label,
         # get name of function and remove "table_" from it
         topic_label = gsub("table_", "", tail(as.character(sys.call()[[1]]), n = 1)),
         fig_or_table = "table",
@@ -75,9 +98,10 @@ table_projections <- function(
         dir = tables_dir,
         scale_amount = 1,
         unit_label = unit_label,
-        table_df = final
+        table_df = final_table
       )
     }
 
-    return(final)
+  # Send table(s) to viewer
+  return(final_table)
 }
