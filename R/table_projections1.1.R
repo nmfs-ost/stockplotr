6e5229d0d87b74b7d28d2f152017f9ae88f99b24
Sam Schiano <125507018+Schiano-NOAA@users.noreply.github.com>
2026-07-17 10:50:06 -0400

    make final combination of dfs and final table

--- R/table_projections1.1.R
+++ R/table_projections1.1.R
@@ -48,58 +48,28 @@ table_projections <- function(
         dplyr::mutate(estimate = round(as.numeric(estimate), digits = 0)) |>
         dplyr::mutate(uncertainty = round(as.numeric(uncertainty), digits = 2))
       # process to reduce colums
-      # processed_data <- 
-      process_table(filtered_data)
-      # data <- processed_data[[1]][[1]]
-      # group <- processed_data[[2]]
+      processed_data <- process_table(filtered_data,
+                                      digits = ifelse(x == "fishing_mortality", 5, 2))
+      data <- processed_data[[1]]
+      group <- processed_data[[2]]
+      # TODO: extract uncert lab from uncert col in processed_data
+      # final df to merge
+      merge_error(
+        table_data = data,
+        id_col_vals = group,
+        unit_label = unit_label[grepl(x, names(unit_label))][[1]],
+        uncert_lab = uncertainty[grepl(x, names(uncertainty))][[1]]
+      )[[1]]
     }
   )
   
-  # TODO: extract uncert lab from uncert col in processed_data
-  # final df to merge
-  final <- merge_error(
-    table_data = lab_list,
-    id_col_vals = group,
-    unit_label = unit_label[grepl(x, names(unit_label))][[1]],
-    uncert_lab = uncertainty[grepl(x, names(uncertainty))][[1]]
-  )
-  # TODO: cbind columns with year/matching groups
-  # TODO: add to gt and add_theme
+  combine_data <- purrr::reduce(lab_list, dplyr::full_join, by = c("Year")) |>
+    gt::gt()
+  
+  final_table <- add_theme(combine_data)
+  
   # TODO: add step for exporting as rda
   
-  # this is what our processing should achieve
-  # proj_raw <- dat |> dplyr::filter(module_name == "projections")
-  # proj_wide <- proj_raw |>
-  #   dplyr::select(year, label, estimate_chr) |>
-  #   dplyr::distinct(year, label, .keep_all = TRUE) |>
-  # TODO: pivot names with a setup like the code below (pivot_wider): year is a col, then labels are individual cols
-    # tidyr::pivot_wider(names_from = label,
-    #                    values_from = estimate_chr)
-  
-  # proj_data <- proj_wide |>
-  #   dplyr::mutate(Period = dplyr::case_when(
-  #     year == min(year, na.rm = TRUE) ~ "Assumed", 
-  #     TRUE ~ "Projected")) |>
-  #   dplyr::select(Period, Year = year, `Catch (mt)` = total_catch, `SSB (mt)` = biomass, F_VAL = fishing_mortality)
-  
-  # Build table
-  final_table <- test |>
-    gt::gt(groupname_col = "Period", id = label) |>
-    gt::cols_align(align = "right", columns = dplyr::everything()) |>
-    gt::cols_align(align = "left", columns = Year) |>
-    gt::cols_label(F_VAL = if (is_latex) gt::md(f_display_label) else gt::html(f_display_label)) |>
-    
-    gt::opt_row_striping() |>
-    gt::tab_options(
-      #table.width = if (is_latex) gt::pct(100) else gt::px(700),
-      table.font.size = if (is_latex) gt::px(12) else gt::px(14),
-      # Use this instead to keep the table compact
-      table.width = if (is_latex) NULL else gt::pct(100),
-      # Ensure data rows aren't overly tall
-      data_row.padding = gt::px(4),
-      row_group.font.weight = "bold",
-      row_group.background.color = "#eeeeee"
-    )
   
   return(final_table)
 }
