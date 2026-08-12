28790eee386b7d296265fbbd29c745dd281d1af3
sbreitbart-NOAA <sophie.breitbart@noaa.gov>
2026-08-12 16:24:40 -0400

    update documentation and fix tests (except for convert_output() r4ss test)

--- tests/testthat/test-plot_biomass_at_age.R
+++ tests/testthat/test-plot_biomass_at_age.R
@@ -1,7 +1,8 @@
 test_that("plot_biomass_at_age generates plots without errors", {
   # expect error-free plot with minimal arguments
   expect_no_error(
-    plot_biomass_at_age(stockplotr::example_data,module = "BIOMASS_AT_AGE")
+    plot_biomass_at_age(stockplotr::example_data,
+                        module = "BIOMASS_AT_AGE")
   )
 
   # expect error-free plot with many arguments
@@ -35,6 +36,7 @@ test_that("rda file made when indicated", {
   # export rda
   plot_biomass_at_age(
     stockplotr::example_data,
+    module = "BIOMASS_AT_AGE",
     figures_dir = getwd(),
     make_rda = TRUE
   )
@@ -129,6 +131,7 @@ test_that("plot_biomass_at_age generates error when biomass label is not found",
 test_that("plot is made proportional and does not contain a legend", {
   plot <- plot_biomass_at_age(
     stockplotr::example_data,
+    module = "BIOMASS_AT_AGE",
     unit_label = "fish",
     proportional = TRUE,
     scale_amount = 1,
@@ -141,6 +144,7 @@ test_that("plot is made proportional and does not contain a legend", {
 test_that("plot contains a legend", {
   plot <- plot_biomass_at_age(
     stockplotr::example_data,
+    module = "BIOMASS_AT_AGE",
     proportional = FALSE,
     unit_label = "fish",
     scale_amount = 1,
--- tests/testthat/test-plot_catch_comp.R
+++ tests/testthat/test-plot_catch_comp.R
@@ -2,7 +2,8 @@
 test_that("plot_catch_comp generates plots without errors", {
   # expect error-free plot with minimal arguments
   expect_no_error(
-    plot_catch_comp(stockplotr::example_data, module = "CATCH_AT_AGE")
+    plot_catch_comp(stockplotr::example_data, 
+                    module = "CATCH_AT_AGE")
   )
 
   # expect error-free plot with many arguments
@@ -36,6 +37,7 @@ test_that("rda file made when indicated", {
   # export rda
   plot_catch_comp(
     stockplotr::example_data,
+    module = "CATCH_AT_AGE",
     figures_dir = getwd(),
     make_rda = TRUE
   )
@@ -144,6 +146,7 @@ test_that("plot is made proportional and does not contain a legend", {
   plot <- plot_catch_comp(
     stockplotr::example_data,
     unit_label = "fish",
+    module = "CATCH_AT_AGE",
     proportional = TRUE,
     scale_amount = 1,
     make_rda = FALSE,
@@ -156,6 +159,7 @@ test_that("plot contains a legend", {
   plot <- plot_catch_comp(
     stockplotr::example_data,
     proportional = FALSE,
+    module = "CATCH_AT_AGE",
     unit_label = "fish",
     scale_amount = 1,
     make_rda = FALSE,
@@ -169,6 +173,7 @@ test_that("year class lines are plotted", {
     stockplotr::example_data,
     proportional = FALSE,
     unit_label = "fish",
+    module = "CATCH_AT_AGE",
     scale_amount = 1,
     make_rda = FALSE,
     figures_dir = getwd()
